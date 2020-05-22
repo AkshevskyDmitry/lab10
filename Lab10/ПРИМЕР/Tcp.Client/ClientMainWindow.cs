43d250be541beadb4c9cd6eb61f9c4228f8ef92f
AkshevskyDmitry <46112874+AkshevskyDmitry@users.noreply.github.com>
2020-05-22 16:31:28 +0300

    Add files via upload

---
+++ Lab10/ПРИМЕР/Tcp.Client/ClientMainWindow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using SomeProject.Library.Client;
+using SomeProject.Library;
+
+namespace SomeProject.TcpClient
+{
+    public partial class ClientMainWindow : Form
+    {
+        public ClientMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработчик кнопки для отправки сообщения на сервер
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnMsgBtnClick(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            Result res = client.SendMessageToServer(textBox.Text).Result;
+            
+            serverOutput.Text += "\n" + client.ReceiveMessageFromServer().Message;
+            
+            if(res == Result.OK)
+            {
+                textBox.Text = "";
+                labelRes.Text = "Message was sent succefully!";
+            }
+            else
+            {
+                labelRes.Text = "Cannot send the message to the server.";
+            }
+            timer.Interval = 2000;
+            timer.Start();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            labelRes.Text = "";
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// Обработчик кнопки для отправки фалйа на сервер
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendFileBtn_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.Yes || dialogResult == DialogResult.OK)
+            {
+                var client = new Client();
+                var res = client.SendFileToServer(openFileDialog.FileName).Result;
+            
+                serverOutput.Text += "\n" + client.ReceiveMessageFromServer().Message;
+            
+                if(res == Result.OK)
+                {
+                    textBox.Text = "";
+                    labelRes.Text = "File was sent succefully!";
+                }
+                else
+                {
+                    labelRes.Text = "Cannot send the file to the server.";
+                }
+                timer.Interval = 2000;
+                timer.Start();
+            }
+        }
+    }
+}
